3061267a79786a32c2bef721a2984c42a1bfad72
Josiah Schwab <jschwab@gmail.com>
2015-02-08 18:37:06 -0800

    update keybindings

--- xbindkeysrc.scm
+++ xbindkeysrc.scm
@@ -4,14 +4,17 @@
 
 ;; bind audio control keys
 (xbindkey '("XF86AudioMute") "amixer sset Master toggle")
-(xbindkey '("XF86AudioLowerVolume") "amixer sset Master playback 5dB-")
-(xbindkey '("XF86AudioRaiseVolume") "amixer sset Master playback 5dB+")
+(xbindkey '("XF86AudioLowerVolume") "amixer sset Master 5%-")
+(xbindkey '("XF86AudioRaiseVolume") "amixer sset Master 5%+")
 (xbindkey '("XF86AudioMicMute") "amixer sset Mic toggle")
 
 ;; bind monitor switching to F11
 (xbindkey '("F11") "autorandr --change")
 ;; bind monitor switching to F11 on laptop keyboard
-(xbindkey '("c:248") "autorandr --change")
+(xbindkey '("XF86LaunchA") "autorandr --change")
+
+;; take a screenshot
+(xbindkey '("XF86MyComputer") "/home/jschwab/scripts/screenshot")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; End of xbindkeys guile configuration ;;
