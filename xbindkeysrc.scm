2bd34f0f6138c8a02b53e29283c5158fce631f41
Josiah Schwab <jschwab@gmail.com>
2016-10-17 17:28:51 -0700

    Give up and bind brightness keys too

--- xbindkeysrc.scm
+++ xbindkeysrc.scm
@@ -8,6 +8,10 @@
 (xbindkey '("XF86AudioRaiseVolume") "amixer sset Master 5%+")
 (xbindkey '("XF86AudioMicMute") "amixer sset Mic toggle")
 
+;; bind brightness control keys
+(xbindkey '("XF86MonBrightnessDown") "xbacklight -dec 5")
+(xbindkey '("XF86MonBrightnessUp") "xbacklight -inc 5")
+
 ;; bind monitor switching to F11
 (xbindkey '("F11") "autorandr --change")
 ;; bind monitor switching to F11 on laptop keyboard
